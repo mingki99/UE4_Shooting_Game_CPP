79a61622b33dd41c3ef77f68e839dc9c2ec41add
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-07-21 01:18:27 +0900

    .gitIgnore

--- ShootingGame_CPP/Intermediate/Build/Win64/ShootingGame_CPPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30139
-#include "D:/File/UE4_Shooting_Game_CPP/ShootingGame_CPP/Intermediate/Build/Win64/ShootingGame_CPPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- ShootingGame_CPP/Source/ShootingGame_CPP/private/ShootingGame_CPP.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShootingGame_CPP.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShootingGame_CPP, "ShootingGame_CPP" );
- 
