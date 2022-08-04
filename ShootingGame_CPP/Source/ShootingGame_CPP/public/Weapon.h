d8e331132b40665d3ab67495af53395a3b7d9d10
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-08-04 21:59:40 +0900

    pix Character.cpp - Modifying the position of the launch With LineTrace -- Weapon Socket("Muzzle") Vector Center [start, End]
    Add
    Colision = "Item"
    Weapon SetColision("Item")

--- ShootingGame_CPP/Source/ShootingGame_CPP/public/Weapon.h
+++ ShootingGame_CPP/Source/ShootingGame_CPP/public/Weapon.h
@@ -19,12 +19,20 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	virtual void PostInitializeComponents() override;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	UFUNCTION()
+	void OnCharacterOverlap(UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
 public:
-	
+
 	UPROPERTY()
 	int32 Damage;
 
